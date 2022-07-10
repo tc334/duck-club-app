32d6ce735d4a47a8329f80a03c7d574385f33fcb
Tegan Counts <tegan@gh.st>
2022-07-09 23:27:21 -1000

    Attempting to connect to the real db

--- app/views.py
+++ app/views.py
@@ -5,9 +5,9 @@ from .apis.auth_wraps import token_required, admin_only
 main = Blueprint('main', __name__)
 
 
-# @main.route('/')
-# def main_index():
-#     return "Hello World!"
+@main.route('/')
+def main_index():
+    return "Hello World, from the real app!"
 
 
 @main.route('/nuke_db')
