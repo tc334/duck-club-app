435d1e54cbece78a1eeb136807b5a8106ce9911c
Tegan <tegan.counts@gmail.com>
2022-08-06 10:03:07 -0500

    correcting DB URL for frontend

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,3 +1,3 @@
-//export const base_uri = "https://boiling-crag-66497.herokuapp.com"; // Heroku
+export const base_uri = "https://boiling-crag-66497.herokuapp.com"; // Heroku
 //export const base_uri = "http://192.168.1.74:5000"; // home
-export const base_uri = "http://127.0.0.1:5000"; // home
+//export const base_uri = "http://127.0.0.1:5000"; // home
